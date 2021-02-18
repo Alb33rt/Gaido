e25e8668a5cd92d96d8963cd568e223d21c91023
Alb33rt <albert931023@gmail.com>
2021-02-18 17:47:39 +0800

    added Pixel Lite UI kit

--- GaidoMain/settings.py
+++ GaidoMain/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 
@@ -126,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'pixel-lite-ui')
+    )
+
+STATIC_ROOT = "../pixel-lite-ui"
