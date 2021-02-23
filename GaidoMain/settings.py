18d07c64affb477cad247dab80c40352f03be19e
Alb33rt <albert931023@gmail.com>
2021-02-24 00:21:21 +0800

    updated settings.py

--- GaidoMain/settings.py
+++ GaidoMain/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
+STATIC_URL = STATIC_HOST +'/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
