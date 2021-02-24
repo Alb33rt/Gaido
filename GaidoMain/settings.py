17fba1f0d41b36026151c6c3842e270f64afd29e
Alb33rt <albert931023@gmail.com>
2021-02-24 08:57:07 +0800

    remove dist deletion from gitignore

--- GaidoMain/settings.py
+++ GaidoMain/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
-STATIC_URL = STATIC_HOST + '/static/'
+STATIC_URL = STATIC_HOST + '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
